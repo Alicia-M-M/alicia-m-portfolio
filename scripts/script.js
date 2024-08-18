2f45c8192b52b82080cbe3d47a4c7713cfcd0bfc
Alicia Morley <aliciaseal81@gmail.com>
2024-08-18 13:45:48 -0400

    basic layout set up

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    // date on footer
+    var date = new Date().getFullYear();
+    document.getElementById("date-info").innerHTML = date;
+
+    
+});
+
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
