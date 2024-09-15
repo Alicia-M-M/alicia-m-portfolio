94a4d6cce0a5d7c4cb4576fb64dac97795e83bfb
Alicia Morley <aliciaseal81@gmail.com>
2024-09-14 20:30:51 -0400

    updated bootstrap older version

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,10 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-
-    // date on footer
-    var date = new Date().getFullYear();
-    document.getElementById("date-info").innerHTML = date;
-
-    
-});
-
 // $( document ).ready(function() {
 //     console.log( "ready!" );
+
+
+//     var date = new Date().getFullYear();
+//     document.getElementById("date-info").innerHTML = date;
+
+    
 // });
+
